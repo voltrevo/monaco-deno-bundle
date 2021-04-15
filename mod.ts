5e4728fc292f768434afbc411aa11eb457a4d693
Andrew Morris <voltrevo@gmail.com>
2021-04-15 15:10:45 +1000

    Move files around

--- mod.ts
+++ mod.ts
@@ -1,1 +1,21 @@
-export * from "./src/index.ts";
+import './deps/monaco-editor-0.23.0/vs/editor/editor.all.js';
+
+export * from './deps/monaco-editor-0.23.0/vs/editor/editor.api.js';
+
+(self as any).MonacoEnvironment = {
+  getWorkerUrl (/* moduleId, label */) {
+    // if (label === 'json') {
+    //   return './json.worker.bundle.js';
+    // }
+    // if (label === 'css') {
+    //   return './css.worker.bundle.js';
+    // }
+    // if (label === 'html') {
+    //   return './html.worker.bundle.js';
+    // }
+    // if (label === 'typescript' || label === 'javascript') {
+    //   return './ts.worker.bundle.js';
+    // }
+    return '/editor.worker.bundle.js';
+  },
+};
